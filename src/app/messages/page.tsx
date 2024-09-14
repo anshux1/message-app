c0dc32108e14878d0a347bd2877c838f28667852
anshux1 <anshukrsingh11@gmail.com>
2024-09-14 22:18:01 +0530

    fixed auto height for messages

--- src/app/messages/page.tsx
+++ src/app/messages/page.tsx
@@ -3,7 +3,7 @@ import SendMessages from '@/components/SendMessage'
 
 export default function HomePage(){ 
   return (
-    <div className="bg-background relative flex size-full w-screen h-screen pt-16 items-center justify-center overflow-hidden rounded-lg border" >
+    <div className="bg-background relative flex size-full w-screen h-auto pt-16 items-center justify-center overflow-hidden rounded-lg border" >
       <div className="size-full flex flex-col items-start max-w-5xl overflow-hidden">
         <SendMessages />
         <Messages />
