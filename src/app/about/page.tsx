1119899a1d2601a5485463c55c003972f8e4abcb
Anshu Kumar Singh <anshukrsingh11@gmail.com>
2024-09-26 11:02:46 +0530

    Updated about message

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,7 +3,7 @@ import RadialGradient from '@/components/ui/radial-gradient'
 const content = [
   { key: 1, Heading: "About us", data: "Welcome to VeliChat, the place where you can express yourself freely and send messages anonymously to anyone. Whether it's sharing your thoughts, sending a compliment, or leaving an encouraging note, our platform gives you a way to communicate without revealing your identity." },
   { key: 2, Heading: "Our mission", data: "We believe in the power of words and the importance of free expression. Our goal is to create a platform that promotes communication, kindness, and understanding while maintaining privacy." },
-  { key: 3, Heading: "Safety & Respect", data: "We are committed to ensuring that [Your Website Name] remains a positive space. While we allow anonymous messaging, we have strict guidelines against hate speech, harassment, or harmful behavior. We encourage users to communicate respectfully and responsibly."}
+  { key: 3, Heading: "Safety & Respect", data: "We are committed to ensuring that Velichat remains a positive space. While we allow anonymous messaging, we have strict guidelines against hate speech, harassment, or harmful behavior. We encourage users to communicate respectfully and responsibly."}
 ]
 
 export default function HomePage() {
